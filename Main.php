80f9ed6e1bf4d054793b3b4997cc4b51560d58b7
JoeWood <JoeWood@JoeWoods-MacBook-Pro.local>
2016-10-24 22:57:47 +0100

    creation of Main class.

---
+++ Main.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoeWood
+ * Date: 24/10/2016
+ * Time: 22:37
+ */
+
+
+class Main {
+
+
+
+    public function makeChecksum(){
+
+
+
+    }
+
+
+
+
+
+}
+
+$m = new Main();
